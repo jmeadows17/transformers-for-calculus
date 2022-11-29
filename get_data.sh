ad7ad33f00273d59a19a3552f394d59682dc8478
mValentino91 <marco@DESKTOP-8HJ11VO.localdomain>
2022-11-29 11:59:28 +0000

    update data download

--- get_data.sh
+++ get_data.sh
@@ -2,3 +2,5 @@
 
 pip install -U gdown
 gdown https://drive.google.com/uc?id=1MqGd9ZUFH05JO8iWldgg6xpe1jNLTI2P
+gdown https://drive.google.com/uc?id=1etAxe1o-6l0pKy_Z-aEG4_3l-vR37_KI
+gdown https://drive.google.com/uc?id=1opo7fFna3Kjtgh7OvdQxGifuUursDt3k
