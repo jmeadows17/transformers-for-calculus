15615d8543da3171c1f2b8b0b2bd00aaebd29801
mValentino91 <marco@DESKTOP-8HJ11VO.localdomain>
2022-12-01 13:21:22 +0000

    download data

--- get_data.sh
+++ get_data.sh
@@ -2,5 +2,6 @@
 
 pip install -U gdown
 gdown https://drive.google.com/uc?id=1MqGd9ZUFH05JO8iWldgg6xpe1jNLTI2P
-gdown https://drive.google.com/uc?id=1etAxe1o-6l0pKy_Z-aEG4_3l-vR37_KI
 gdown https://drive.google.com/uc?id=1opo7fFna3Kjtgh7OvdQxGifuUursDt3k
+gdown https://drive.google.com/uc?id=1XX5gROyIOyM9H_SuieK4g2FE-zyqhTiU
+gdown https://drive.google.com/uc?id=1zyvMFVszdlBa7ZUdKDZSPN_98TPmUq2K
